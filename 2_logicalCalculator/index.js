839e3643c79e9fbd8d1dc869c69bce245c6b56bc
Victor Pipin <parabletraveller@gmail.com>
2025-01-19 15:23:18 +0300

    eat: added task specification
    Added a task condition to implement a function that returns a Boolean result based on sequential operator application to values in an array.

--- 2_logicalCalculator/index.js
+++ 2_logicalCalculator/index.js
@@ -1,1 +1,45 @@
-// Добавить цифры в названия папок
+
+// ❓TASK:
+// Даны массив булевых значений и логический оператор,
+// возвращает булевый результат, основанный на последовательном применении оператора к значениям в массиве.
+
+// 🔽INPUT:
+// 1. Массив булевых значений (1 <= array_length <= 50)
+// 2. Строка, определяющая логический оператор: "AND", "OR", "XOR"
+
+// 🔼OUTPUT:
+// Булево значение (True or False).
+
+// 💡EXAMPLES:
+// operator: 'AND'
+// booleans = [True, True, False], operator = "AND"
+// True AND True -> True
+// True AND False -> False
+// return False
+
+// operator: 'OR'
+// booleans = [True, True, False], operator = "OR"
+// True OR True -> True
+// True OR False -> True
+// return True
+
+// operator: 'XOR'
+// booleans = [True, True, False], operator = "XOR"
+// True XOR True -> False
+// False XOR False -> False
+// return False
+
+// 🔄ENGLISH TRANSLATION:
+// Given an array of Boolean values and a logical operator,
+// return a Boolean result based on sequentially applying the operator to the values in the array.
+// 🔽INPUT:
+// 1. An Array of boolean values (1 <= array_length <= 50)
+// 2. A String defining a logical operator: "AND", "OR", "XOR"
+// 🔼OUTPUT:
+// A Boolean value (True or False).
+
+
+// ✅SOLUTION:
+
+
+// 📈REFACTOR:
